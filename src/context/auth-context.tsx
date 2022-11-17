d62d698ea52233d504f51ba7546d9edd53c4cc57
npsopheak7 <sean@slash.co>
2022-11-17 16:32:37 +0700

    fix set token on check token

--- src/context/auth-context.tsx
+++ src/context/auth-context.tsx
@@ -2,55 +2,63 @@ import React from "react";
 import { useRouter } from "next/router";
 
 type AuthContextInterface = {
-  authState: AuthData,
-  isUserAuthenticated?: Function,
-  setAuthState?: Function,
-}
+  authState: AuthData;
+  isUserAuthenticated?: Function;
+  setAuthState?: Function;
+};
 export type AuthData = {
-  token: string,
-}
+  token: string;
+};
 
-const defaultState: AuthContextInterface  = {
+const defaultState: AuthContextInterface = {
   authState: {
-    token: ""
+    token: "",
   },
-}
+};
 
 const AuthContext = React.createContext<AuthContextInterface>(defaultState);
 const { Provider } = AuthContext;
 
 const AuthProvider = ({ children }: any) => {
   const [authState, setAuthState] = React.useState({
-   token: "",
+    token: "",
   });
 
   const setUserAuthInfo = ({ token }: AuthData) => {
-   localStorage.setItem("token", token);
+    localStorage.setItem("token", token);
 
-   setAuthState({
-    token,
-   });
- };
+    setAuthState({
+      token,
+    });
+  };
 
- // checks if the user is authenticated or not
- const isUserAuthenticated = ():boolean => {
-  if (!authState.token) {
-    return false;
-  }
-  return true;
- };
+  // checks if the user is authenticated or not
+  const isUserAuthenticated = (): boolean => {
+    const initialAuthData: AuthData = {
+      token: localStorage.getItem("token") ?? "",
+    };
+    if (initialAuthData.token) {
+      setAuthState({
+        token: initialAuthData.token,
+      });
+    }
+    if (!authState.token && !initialAuthData.token) {
+      return false;
+    }
+    return true;
+  };
 
- return (
-   <Provider
-     value={{
-      authState,
-      setAuthState: (userAuthInfo: AuthData) => setUserAuthInfo(userAuthInfo),
-      isUserAuthenticated,
-    }}
-   >
-    {children}
-   </Provider>
- );
+  return (
+    <Provider
+      value={{
+        authState,
+        setAuthState: (userAuthInfo: AuthData) => setUserAuthInfo(userAuthInfo),
+        isUserAuthenticated,
+      }}
+    >
+      {children}
+    </Provider>
+  );
 };
 
 export { AuthContext, AuthProvider };
