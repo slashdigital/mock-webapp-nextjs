a1c3b88260870b674d0628c2fa11a6ba85fd3c98
npsopheak7 <sean@slash.co>
2022-11-17 16:22:50 +0700

    add simple three screen flow

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,7 @@
 import '../styles/globals.css'
 import type { AppProps } from 'next/app'
+import { AuthProvider } from '../src/context/auth-context'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return <AuthProvider> <Component {...pageProps} /></AuthProvider>;
 }
---
+++ src/context/auth-context.tsx
@@ -0,0 +1,56 @@
+import React from "react";
+import { useRouter } from "next/router";
+
+type AuthContextInterface = {
+  authState: AuthData,
+  isUserAuthenticated?: Function,
+  setAuthState?: Function,
+}
+export type AuthData = {
+  token: string,
+}
+
+const defaultState: AuthContextInterface  = {
+  authState: {
+    token: ""
+  },
+}
+
+const AuthContext = React.createContext<AuthContextInterface>(defaultState);
+const { Provider } = AuthContext;
+
+const AuthProvider = ({ children }: any) => {
+  const [authState, setAuthState] = React.useState({
+   token: "",
+  });
+
+  const setUserAuthInfo = ({ token }: AuthData) => {
+   localStorage.setItem("token", token);
+
+   setAuthState({
+    token,
+   });
+ };
+
+ // checks if the user is authenticated or not
+ const isUserAuthenticated = ():boolean => {
+  if (!authState.token) {
+    return false;
+  }
+  return true;
+ };
+
+ return (
+   <Provider
+     value={{
+      authState,
+      setAuthState: (userAuthInfo: AuthData) => setUserAuthInfo(userAuthInfo),
+      isUserAuthenticated,
+    }}
+   >
+    {children}
+   </Provider>
+ );
+};
+
+export { AuthContext, AuthProvider };
